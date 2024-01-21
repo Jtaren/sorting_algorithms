914e6d6a69e2b89a7d3a766dccb5332a483ac5a7
jtaren <arenjatau@gmail.com>
2024-01-21 08:46:16 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,25 @@
 #include "sort.h"
 
-void swap(int *array, size_t ind_1, size_t ind_2);
-
 /**
- * bubble_sort - performs bubble sort on an array
- * @array: array to sory (of ints)
- * @size: size of array (as size_t)
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void bubble_sort(int *array, size_t size)
-{
-	BOOL sorted = FALSE;
-	size_t i, sorted_i = size - 1;
 
-	if (!array || size <= 1)
-		return;
-	while (!sorted)
-	{
-		sorted = TRUE;
-		for (i = 0; i < sorted_i; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap(array, i, i + 1);
-				sorted = FALSE;
-				print_array(array, size);
-			}
-		}
-		sorted_i--;
-	}
-}
-
-/**
- * swap - swaps array index elements
- * @array: array to swap in
- * @ind_1: index 1 to swap with index 2
- * @ind_2: index 2 to swap with index 1
- */
-void swap(int *array, size_t ind_1, size_t ind_2)
-{
-	int temp;
-
-	temp = array[ind_1];
-	array[ind_1] = array[ind_2];
-	array[ind_2] = temp;
+void bubble_sort(int *array, size_t size) {
+  for (size_t i = 0; i < size - 1; i++) {
+    int swapped = 0;
+    for (size_t j = 0; j < size - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped = 1;
+        printf("Swapped %d and %d\n", array[j + 1], array[j]);
+      }
+    }
+    if (!swapped) {
+      break; // Array is already sorted
+    }
+  }
 }
